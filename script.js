ccbeb367bdcbdb687272e398392f63fa1d05070e
Levani migriauli <levani.migriauli@lb.ge>
2024-12-19 23:56:32 +0400

    refactor

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict'
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.btn--close-modal')
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal')
+const btnScrollTo = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault()
@@ -32,8 +34,6 @@ document.addEventListener('keydown', function (e) {
 
 ///////////////////////////////////////
 // Smooth scrolling
-const btnScrollTo = document.querySelector('.btn--scroll-to')
-const section1 = document.querySelector('#section--1')
 
 btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({
