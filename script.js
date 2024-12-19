a3cc875f0cc1214b43e2cc5d1458620043db6b6f
Levani migriauli <levani.migriauli@lb.ge>
2024-12-19 23:54:48 +0400

    feat: add smooth scroll on Learn more

--- script.js
+++ script.js
@@ -28,4 +28,15 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal()
   }
-});
+})
+
+///////////////////////////////////////
+// Smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
+
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({
+    behavior: 'smooth'
+  })
+})
